216f1f79ff59585ff3eaec02beee97958cdd7ce8
Wer <davigwerneck@gmail.com>
2024-04-16 16:37:39 -0300

    ADDING SOME ASSETS FOR FUN

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,8 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if velocity.y >500:
+			velocity.y = 500
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
