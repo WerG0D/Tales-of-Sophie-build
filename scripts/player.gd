c35c659874c4833a770deecf32c55097cf89dc21
Wer <davigwerneck@gmail.com>
2024-04-17 17:58:29 -0300

    Coding jump mechanic, W.I.P, Disabled till next commit

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+const JUMP_TIME = 0.35
+const JUMP_RELEASE_MULTIPLIER = 0.5
+const MAX_FALL_SPEED = 500.0
+const MAX_JUMP_TIME = 0.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var jump_timer = 0
+var jump_pressed = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,8 +21,19 @@ func _physics_process(delta):
 			velocity.y = 500
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var input_y = Input.get_action_strength("jump")
+	if is_on_floor() and input_y > 0:
+		jump_pressed = true
+		jump_timer = 0
+	if jump_pressed and jump_timer < JUMP_TIME:
+		velocity.y = lerp(0.0, JUMP_VELOCITY, jump_timer / MAX_JUMP_TIME)
+		jump_timer += delta
+	else:
+		jump_pressed = false
+	if not jump_pressed and velocity.y < 0:
+		velocity.y += gravity * JUMP_RELEASE_MULTIPLIER * delta
+	if velocity.y > MAX_FALL_SPEED:
+		velocity.y = MAX_FALL_SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
