8b5134af8f64cfc3f9e466ee4c4567702bf43b8d
Wer <davigwerneck@gmail.com>
2024-05-03 09:39:50 -0300

    changing project structure

--- scripts/world.gd
+++ scripts/world.gd
@@ -5,27 +5,27 @@ var is_openingcutscene = false
 var has_player_entered_area = false
 var player = null
 var is_pathfollowing = false
-var torch_has_happened = false
-var torch_is_happening = false 
+var objects_has_happened = false
+var objects_is_happening = false 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
 	if is_openingcutscene:
 		var pathfollower = $Path2D/PathFollow2D
 		
 		if is_pathfollowing:
-			if !torch_is_happening:
+			if !objects_is_happening:
 				pathfollower.progress_ratio += 0.001
 			
 			if pathfollower.progress_ratio >= 1:
 				cutsceneendind()
 				
-			if !torch_has_happened and pathfollower.progress_ratio >= 0.768 and !torch_is_happening:
-				torch_is_happening = true
+			if !objects_has_happened and pathfollower.progress_ratio >= 0.768 and !objects_is_happening:
+				objects_is_happening = true
 				await get_tree().create_timer(1).timeout
 				$Objects.visible = true
 				await get_tree().create_timer(0.5).timeout
-				torch_has_happened = true
-				torch_is_happening = false 
+				objects_has_happened = true
+				objects_is_happening = false 
 				
 				 
 
