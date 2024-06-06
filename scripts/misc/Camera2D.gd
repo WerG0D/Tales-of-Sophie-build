f7be05643abf7a4620f9e01dda92e0ecf8ab575e
Gabriel Duarte Guerra <gabrielduarteg02@gmail.com>
2024-06-06 12:00:21 -0300

    wipest wipe of the wips

--- scripts/misc/Camera2D.gd
+++ scripts/misc/Camera2D.gd
@@ -3,11 +3,11 @@ extends Camera2D
 const DEAD_ZONE = 1
 
 func _input(event: InputEvent) -> void:
+	var staticpos = self.position
 	if event is InputEventMouseMotion:
 		var _target = event.position - get_viewport().size * 0.5
 		if _target.length() < DEAD_ZONE:
 			self.position = Vector2(0,0)
 		else:
-
 			self.position = _target.normalized() * (_target.length() - DEAD_ZONE) * 0.1
 ########TODO (gabriel) testar se é de boa adicionar um lerp pra camera tiltar com a velocidade ia ser pog
--- scripts/misc/ParallaxBg.gd
+++ scripts/misc/ParallaxBg.gd
@@ -1,21 +1,27 @@
 extends Line2D
 @onready var camera = $"../player/Camera2D"
+@onready var player = $"../player/Sprite2D"
 @export var parallaxvelocityX: float #SO LEMBRAR QUE É INVERTIDO!!!
 @export var parallaxvelocityY: float
 @export var parallaxsize: int
 @export var parallaxheight: float
 @export var isshaded: bool
 var camerainit = Vector2()
+var camvec = Vector2(1,1)
+
 func _ready():
 	camerainit = camera.global_position
 func _physics_process(delta):
 	if camera != null:
-		camera = $"../player/Camera2D"
-		self.set_point_position(0,Vector2(to_local(camera.global_position).x + parallaxheight  , to_local(camera.global_position).y -parallaxsize))
-		self.set_point_position(1,Vector2(to_local(camera.global_position).x + parallaxheight, to_local(camera.global_position).y +parallaxsize))
+		player = $"../player/Sprite2D"
+		self.set_point_position(0,Vector2(to_local(player.global_position).x + parallaxheight  ,to_local(player.global_position).y -parallaxsize))
+		self.set_point_position(1,Vector2(to_local(player.global_position).x + parallaxheight, to_local(player.global_position).y +parallaxsize))
 		if isshaded:
-			var camvec = Vector2(-to_local(camera.global_position).y,to_local(camera.global_position).x)
+			camvec = Vector2(to_local(player.global_position).y / parallaxvelocityX, to_local(player.global_position).x/ parallaxvelocityY)
+
+			#var camvec = Vector2(to_local(camera.global_position).y / parallaxvelocityX, -to_local(camera.global_position).x / parallaxvelocityY)
 			self.material.set_shader_parameter("camlocaloffset",camvec  )
+			print("X:", camvec)
 
 
 
