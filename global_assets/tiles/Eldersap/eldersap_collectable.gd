eb4e077f2c68b0da2a93b009417331c88a5de078
Wer <51096748+WerG0D@users.noreply.github.com>
2024-07-16 16:45:56 -0300

    fixing eldersap tree

--- global_assets/tiles/Eldersap/eldersap_collectable.gd
+++ global_assets/tiles/Eldersap/eldersap_collectable.gd
@@ -2,10 +2,10 @@ extends StaticBody2D
 const SHINE_TIME = 1.0
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	fallfromtree()
 	
-func fallfromtree():
+func fallfromtree() -> void:
 	$AnimationPlayer.play("sapfall")
 	await get_tree().create_timer(1.5).timeout
 	print("+1 ElderSap")
--- global_assets/tiles/Eldersap/eldersap_tree.gd
+++ global_assets/tiles/Eldersap/eldersap_tree.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-var state = "no sap"
-var player_in_area = false
+var state := "no sap"
+var player_in_area := false
 
-var eldersap = preload("res://assets/tiles/Eldersap/eldersap_collectable.tscn")
+var eldersap := preload("res://global_assets/tiles/Eldersap/eldersap_collectable.tscn")
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	if state == "no sap":
 		$growth_timer.start()
 	pass
 
-func _process(_delta):
+func _process(_delta: float) -> void:
 	#$RichTextLabel.set_text(str("pos:",$AnimatedSprite2D.global_position, " | state:", state))
 	if state == "no sap":
 		$AnimatedSprite2D.play("nosap")
@@ -21,25 +21,8 @@ func _process(_delta):
 				state = "no sap"
 				drop_eldersap()
 
-
-
-
-#func _on_pickable_area_body_entered(body):
-	#if body.has_method("player"):
-		#player_in_area = true
-#
-#
-#func _on_pickable_area_body_exited(body):
-	#if body.has_method("player"):
-		#player_in_area = false
-#
-#
-#func _on_growth_timer_timeout():
-	#if state == "no sap":
-		#state = "sap" # Replace with function body.
-
-func drop_eldersap():
-	var eldersap_instance = eldersap.instantiate()
+func drop_eldersap() -> void:
+	var eldersap_instance := eldersap.instantiate()
 	eldersap_instance.global_position = $Marker2D.global_position
 	get_parent().add_child(eldersap_instance)
 	$RichTextLabel.set_text(str("pos:",$Marker2D.global_position))
@@ -52,10 +35,10 @@ func _on_growth_timer_timeout() -> void:
 	if state == "no sap":
 		state = "sap"
 
-func _on_pickable_area_body_entered(body) -> void:
+func _on_pickable_area_body_entered(body: CharacterBody2D) -> void:
 	if body.has_method("player"):
 		player_in_area = true
 
-func _on_pickable_area_body_exited(body) -> void:
+func _on_pickable_area_body_exited(body: CharacterBody2D) -> void:
 	if body.has_method("player"):
 		player_in_area = false
