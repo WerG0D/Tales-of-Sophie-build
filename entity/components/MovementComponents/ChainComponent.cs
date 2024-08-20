60166e1fbc1034adee47d33b7b20de66100852d5
Wer <davigwerneck@gmail.com>
2024-08-20 17:53:10 -0300

    A NEW DAY WILL COME FOR THE HOOK.

--- entity/components/MovementComponents/ChainComponent.cs
+++ entity/components/MovementComponents/ChainComponent.cs
@@ -8,7 +8,8 @@ public partial class ChainComponent : Node2D
     [Export] public int HookSpeed = 150; // Velocidade de movimento do gancho, basicamente o quão rápido ele vai ser disparado e se conectar a parede
     [Export] public int PullForce = 60; // Força de puxão do gancho, recomendo fortemente deixar em 60, quanto maior mais forte o personagem vai ser puxado
     [Export] public string HookInputAction = "hook"; // O input map pra disparar o gancho. Nn sei se tem uma maneira melhor de fzr isso mas blz
-    [Export] public CharacterBody2D Entity; 
+    [Export] public float SwingInfluence = 2f; // Influência do jogador no balanço
+	[Export] public CharacterBody2D Entity; 
     [Export] public VelocityComponent VelocityComponent; 
 
     public Vector2 HookDirection = Vector2.Zero; 
@@ -96,23 +97,31 @@ public partial class ChainComponent : Node2D
     {
         if (IsHooked)
         {
+			Vector2 directionToHook = (HookTipPosition - Entity.GlobalPosition).Normalized();
             float distanceToHook = Entity.GlobalPosition.DistanceTo(HookTipPosition);
-            float calculatedPullForce = HookSpeed * Mathf.Clamp(distanceToHook / MaxChainLength, 1.0f, PullForce);
+            
+			float calculatedPullForce = HookSpeed * Mathf.Clamp(distanceToHook / MaxChainLength, 1.0f, PullForce);
 
             Vector2 pullDirection = ToLocal(HookTipPosition).Normalized();
             Entity.Velocity += pullDirection * calculatedPullForce;
 
-            // fuck it eu resolvi aquele bug chato simplesmente na marra e é isso, tnt faz. Simplesmente seta pra nao ter isso
-            if (Mathf.Sign(Entity.Velocity.X) != Mathf.Sign(pullDirection.X))
+			// Força de tensão da corda (para manter o personagem pendurado)
+        	Vector2 tensionForce = directionToHook * PullForce * (distanceToHook / MaxChainLength);
+			Vector2 gravityEffect = new Vector2(0, 9.8f);
+
+            Vector2 swingForce = new Vector2();
+            if (Input.IsActionPressed("move_right"))
             {
-                Entity.Velocity = new Vector2(0, Entity.Velocity.Y);
+                swingForce = new Vector2(SwingInfluence, 0);
+            }
+            else if (Input.IsActionPressed("move_left"))
+            {
+                swingForce = new Vector2(-SwingInfluence, 0);
             }
 
-            // eu simplesmente acho q isso vai dar problema no futuro, mas isso é coisa pra outra era 
-            if (Entity.Velocity.Y > 0 && pullDirection.Y < 0)
-            {
-                Entity.Velocity = new Vector2(Entity.Velocity.X, 0);
-            }
+            // A combinação de forças cria o movimento pendular
+            Entity.Velocity += tensionForce + gravityEffect + swingForce;
+
         }
         else
         {
